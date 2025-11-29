f825a77c08bceed65ad6ce9e211e0b6a10c27642
Baga6312 <oussema.benayech@gmail.com>
2025-11-29 16:13:25 +0100

    added the chatbot

--- backend/src/main/java/com/tuniway/config/DatabaseConfig.java
+++ backend/src/main/java/com/tuniway/config/DatabaseConfig.java
@@ -1,8 +1,10 @@
 package com.tuniway.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -42,6 +44,7 @@ public class DatabaseConfig {
     private String formatSql;
 
     @Bean
+    @DependsOn("databaseCreator") // This ensures database is created first
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(dbDriverClassName);
