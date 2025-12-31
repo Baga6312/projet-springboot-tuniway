4bca98694366ce81d39c28f24ea311d1ccac5797
Baga6312 <oussema.benayech@gmail.com>
2025-12-31 18:32:42 +0100

    set up some certs

--- frontend/src/app/config/api.config.ts
+++ frontend/src/app/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL = 'https://api.tuniway.me/api';
-export const CHATBOT_API_URL = 'http://chabottuniway.duckdns.org:5000';
+export const CHATBOT_API_URL = 'https://chat.tuniway.me';
 
 
